78a1ec5c42ab04e0f7adac9203309ea9cc06b4ef
Cristi Gutierrez Name <65434648+justbycris@users.noreply.github.com>
2023-01-04 15:36:15 -0500

    day 4

---
+++ app.js
@@ -0,0 +1,32 @@
+// var text = ["frontend developer.", "web engineer.", "graphic designer.", "photographer.", "illustrator.", "web designer.", "content creator.", "digital designer."];
+// var counter = 0;
+// var elem = document.getElementById("changeText");
+// var inst = setInterval(changeWord, 1000);
+
+
+// /*------CHANGE WORDS IN HOME TEXT----------*/
+// function changeWord() {
+//   elem.innerHTML = text[counter];
+//   counter++;
+//   if (counter >= text.length) {
+//     counter = 0;
+//     // clearInterval(inst); // uncomment this to stop refreshing after one cycle
+//   }
+// }
+
+/*------------CHANGE THE HOME IMAGE------------*/
+
+var imagesOptions = [ "Images/NYC-PhotoArt.png", "Images/CoverPhotography.png","Images/Portrait_2.png", "Images/StatueLiberty-DigitalArt.png", "Images/DigitalCollage.png","Images/Portrait.png"];
+var counter2 = 0;
+var imgToChange = document.getElementById("changeImg");
+var inst = setInterval(changeImage, 1000);
+
+
+function changeImage() {
+    imgToChange.src = imagesOptions[counter2];
+    counter2++;
+    if (counter2 >= imagesOptions.length) {
+      counter2 = 0;
+      // clearInterval(inst); // uncomment this to stop refreshing after one cycle
+    }
+  }
