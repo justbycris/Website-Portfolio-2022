946e532e97b9fc58e7ff93bf6eea3dcce231a438
Cristi Gutierrez Name <65434648+justbycris@users.noreply.github.com>
2023-01-16 11:29:30 -0500

    worksection-2

--- app.js
+++ app.js
@@ -30,3 +30,32 @@ function changeImage() {
       // clearInterval(inst); // uncomment this to stop refreshing after one cycle
     }
   }
+
+
+  /*---------------WORK: SLIDE HORIZONTAL PROJECT CONTAINER ----------*/
+  var button = document.getElementById('slide');
+button.onclick = function () {
+    var container = document.getElementById('container');
+    sideScroll(container,'right',25,100,10);
+};
+
+var back = document.getElementById('slideBack');
+back.onclick = function () {
+    var container = document.getElementById('container');
+    sideScroll(container,'left',25,100,10);
+};
+
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
